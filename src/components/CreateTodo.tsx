4706add4937ac7c9442d15c4ce957e5b4dc6f129
Rasweb <Rasmuspalm@fastmail.se>
2022-05-23 16:09:22 +0200

    Add to array functionality done but import is not

--- src/components/CreateTodo.tsx
+++ src/components/CreateTodo.tsx
@@ -1,13 +1,65 @@
-export const CreateTodo = () => {
+import { ChangeEvent, FormEvent, useState } from "react";
+import { Items } from "../models/Items";
+import { todos, setTodos } from "./Todos";
+
+interface ICreateTodo {
+  todos: todos;
+  setTodos: setTodos;
+}
+
+export const CreateTodo = (props: ICreateTodo) => {
+  const [oneTodo, setOneTodo] = useState<Items>({
+    title: "",
+    duration: 0,
+    done: false,
+    id: 0,
+  });
+
+  const handleAdd = (e: ChangeEvent<HTMLInputElement>) => {
+    console.log(e.target.name);
+    if (e.target.type !== "number") {
+      setOneTodo({ ...oneTodo, [e.target.name]: e.target.value });
+    } else {
+      setOneTodo({ ...oneTodo, [e.target.name]: +e.target.value });
+    }
+  };
+
+  const handleSubmit = (e: FormEvent) => {
+    e.preventDefault();
+    console.log(oneTodo);
+    setTodos([...todos, oneTodo]);
+  };
+
   return (
     <>
-      {/* <form>
-        <input type="text" value={title} onChange={handleCreate} />
-        <input type="number" value={duration} onChange={handleCreate} />
-        <input type="text" value={done} onChange={handleCreate} />
-        <input type="number" value={id} onChange={handleCreate} />
-        <button type="submit">Submit</button>
-      </form> */}
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="title"
+          name="title"
+          value={oneTodo.title}
+          onChange={handleAdd}
+        />
+        <br />
+        <input
+          type="number"
+          placeholder="minutes"
+          name="duration"
+          value={oneTodo.duration}
+          onChange={handleAdd}
+        />{" "}
+        <br />
+        <input
+          type="number"
+          placeholder="id"
+          name="id"
+          value={oneTodo.id}
+          onChange={handleAdd}
+        />{" "}
+        <br />
+        <button type="submit">Add</button>
+      </form>
+      <div>Todo: {JSON.stringify(oneTodo)}</div>
     </>
   );
 };
--- src/components/Todos.tsx
+++ src/components/Todos.tsx
@@ -1,5 +1,6 @@
-import { ChangeEvent, useState } from "react";
+import { useState } from "react";
 import { Items } from "./../models/Items";
+import { CreateTodo } from "./CreateTodo";
 import { TodoItems } from "./TodoItems";
 
 export const Todos = () => {
@@ -10,18 +11,6 @@ export const Todos = () => {
     new Items("Read book", 30, false, 4),
     new Items("Sleep", 420, false, 5),
   ]);
-  // Add todo:
-  const [title, setTitle] = useState("");
-  const [duration, setDuration] = useState(0);
-  const [done, setDone] = useState("false");
-  const [id, setId] = useState(0);
-
-  // const handleCreate = () => {
-  //   setDuration(duration);
-  //   setTitle(title);
-  //   setDone(done);
-  //   setId(id);
-  // };
 
   const handleClick = (id: number) => {
     // 1. Create new list using map.
@@ -41,15 +30,6 @@ export const Todos = () => {
     setTodos(res);
   };
 
-  // console.log(done);
-
-  // const handleClick = (id: number) => {
-  //   const notDoneItems = todos.filter((item) =>
-  //     item.id === id ? { ...item, done: true } : item
-  //   );
-  //   setTodos(notDoneItems);
-  // };
-
   const handleDelete = (id: number) => {
     // If id is not the pressed one add to new array.
     const newTodos = todos.filter((item) => item.id !== id);
@@ -59,18 +39,21 @@ export const Todos = () => {
 
   let todoHtml = todos.map((item) => {
     return (
-      //  handleDelete={handleDelete}
-      <TodoItems
-        todos={item}
-        key={item.id}
-        handleDelete={handleDelete}
-        handleClick={handleClick}
-      />
+      <>
+        <TodoItems
+          todos={item}
+          key={item.id}
+          handleDelete={handleDelete}
+          handleClick={handleClick}
+        ></TodoItems>
+      </>
     );
   });
 
   return (
     <>
+      {/* <CreateTodo todos={todos} /> */}
+      <CreateTodo todos={todos} setTodos={setTodos} />
       {todoHtml}
 
       {/* <input type="checkbox" checked={done} onChange={handleClick} /> */}
